d1b648ec4973dc262cfdf7998201220e609a6d5c
yogesh verma <yogeshverma.ee@gmail.com>
2019-06-10 16:56:30 +0530

    initial commit

---
+++ src/main/kotlin/samplehollow/SampleHollowApplication.kt
@@ -0,0 +1,13 @@
+package com.self.samplehollow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleHollowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleHollowApplication.class, args);
+    }
+
+}
